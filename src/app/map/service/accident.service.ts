d9b95d762606686703794d7332dc7491d7ca8443
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-04 09:26:05 -0300

    Implement getAllAccidents in AccidentService

--- src/app/map/service/accident.service.ts
+++ src/app/map/service/accident.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Accident } from '../model/accident';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccidentService {
+  private readonly API_URL = 'http://localhost:8080/api/accidents';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllAccidents(): Observable<Accident[]> {
+    return this.http.get<Accident[]>(this.API_URL);
+  }
 }
