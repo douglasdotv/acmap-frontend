30e2cfa4fad9bf633c10aa44ab9fcaef82de8adc
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-05 13:56:14 -0300

    Integrate navbar and footer into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from "./map/map.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, MapComponent]
+    imports: [CommonModule, RouterOutlet, MapComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'acmap';
