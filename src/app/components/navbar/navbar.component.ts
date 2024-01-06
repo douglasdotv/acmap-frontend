ac2992241cc473184728d7dac3cb68468d1380b3
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-06 10:26:25 -0300

    Enhance navbar form with dynamic dropdown menus for filters
    Replaced input fields for operator, aircraft and category with dropdown menus + dynamic data in NavbarComponent

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AccidentService } from '../../services/accident.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
   searchForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  operators: string[] = [];
+  aircraftTypes: string[] = [];
+  accidentCategories: string[] = [];
+
+  constructor(private fb: FormBuilder, private accidentService: AccidentService) {}
 
   ngOnInit(): void {
     this.initForm();
+    this.loadDropdownData();
   }
 
   private initForm(): void {
@@ -23,6 +30,39 @@ export class NavbarComponent implements OnInit {
       operator: [''],
       aircraftType: [''],
       accidentCategory: [''],
+    });
+  }
+
+  private loadDropdownData(): void {
+    this.fetchOperators();
+    this.fetchAircraftTypes();
+    this.fetchCategories();
+  }
+
+  private fetchOperators(): void {
+    this.accidentService.getOperators().subscribe({
+      next: (operators) => {
+        this.operators = operators;
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  private fetchAircraftTypes(): void {
+    this.accidentService.getAircraftTypes().subscribe({
+      next: (aircraftTypes) => {
+        this.aircraftTypes = aircraftTypes;
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  private fetchCategories(): void {
+    this.accidentService.getCategories().subscribe({
+      next: (categories) => {
+        this.accidentCategories = categories;
+      },
+      error: (err) => console.error(err),
     });
   }
 
