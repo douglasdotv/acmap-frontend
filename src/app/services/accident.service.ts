bc8a871ce1c9e1f240f0ba21b3bcf2aa2ec8688d
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-08 23:52:43 -0300

    Use environment api url on accident.service.ts

--- src/app/services/accident.service.ts
+++ src/app/services/accident.service.ts
@@ -3,32 +3,33 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Accident } from '../models/accident';
 import { SearchForm } from '../models/search-form';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccidentService {
-  private readonly API_URL = 'http://localhost:8080/api/accidents';
+  private readonly API_URL = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   getAllAccidents(): Observable<Accident[]> {
-    return this.http.get<Accident[]>(this.API_URL);
+    return this.http.get<Accident[]>(this.API_URL + '/accidents');
   }
 
   searchAccidents(form: SearchForm): Observable<Accident[]> {
-    return this.http.post<Accident[]>(`${this.API_URL}/search`, form);
+    return this.http.post<Accident[]>(this.API_URL + '/accidents/search', form);
   }
 
   getOperators(): Observable<string[]> {
-    return this.http.get<string[]>(this.API_URL + '/operators');
+    return this.http.get<string[]>(this.API_URL + '/accidents/operators');
   }
 
   getAircraftTypes(): Observable<string[]> {
-    return this.http.get<string[]>(this.API_URL + '/aircraft-types');
+    return this.http.get<string[]>(this.API_URL + '/accidents/aircraft-types');
   }
 
   getCategories(): Observable<string[]> {
-    return this.http.get<string[]>(this.API_URL + '/categories');
+    return this.http.get<string[]>(this.API_URL + '/accidents/categories');
   }
 }
